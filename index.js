32b5ccdf405b667d28726e852a1d481a77ae50f9
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-12-01 08:38:43 +0100

    Feat: add schema for mongoDB

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const db = mongoose.connection;
 //check if the connection is successful
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function () {
-    console.log('Connected to MongoDB');
-}
-);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
