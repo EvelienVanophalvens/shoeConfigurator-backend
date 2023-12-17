39f333e55d09645e23780b71eaa2ab37be179683
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-12-17 19:28:33 +0100

    Fix: delete auth

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -19,8 +19,7 @@ router.get('/:id', passport.authenticate('jwt', { session: false }) , shoesContr
 
 router.patch('/:id', passport.authenticate('jwt', { session: false }) , shoesController.patchShoeById);
 
-// router.delete('/:id', passport.authenticate('jwt', { session: false }) , shoesController.removeShoeById);
-router.delete('/:id', shoesController.removeShoeById);
+router.delete('/:id', passport.authenticate('jwt', { session: false }) , shoesController.removeShoeById);
 
 //export the router
 module.exports = router;
