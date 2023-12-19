d05eb656cf17f9574c9b6349bb23abcb4a25ed31
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-12-19 14:07:42 +0100

    Fix: auth

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 //require the controller
-const userController = require('../../../controllers/Auth');
+const userController = require('../../../controllers/auth');
 
 //handle the post request coming to /api/v1/shoes
 router.post('/signup', userController.signup);
